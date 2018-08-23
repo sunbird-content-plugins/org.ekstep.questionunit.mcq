1cdf23376096e9486eb2342898056dfdf57d373c
Ram Jayaraman <ram.j@funtoot.com>
2018-08-23 14:52:36 +0530

    Fixed the font size and question component issues

--- renderer/template/grid_template.js
+++ renderer/template/grid_template.js
@@ -82,7 +82,7 @@ MCQController.getGridOptionsTemplate = function (options) {
 MCQController.getGridTemplate = function (question) {
   var template =
     org.ekstep.questionunit.backgroundComponent.getBackgroundGraphics() +
-    '<div class="mcq-question-container-grid">\
+    '<div class="mcq-question-container-grid plugin-content-container">\
     <div class="mcq-grid-question-container question-content-container">' +
     org.ekstep.questionunit.questionComponent.generateQuestionComponent() +
     '</div>\
