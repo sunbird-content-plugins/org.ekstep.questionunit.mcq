c7803dfce9e29b3e6bd07d89fe284c70b1fc0c09
sivashanmugam-kannan <sivashanmugam.kannan@funtoot.com>
2018-11-14 07:02:41 +0530

    #Issue SB-8961, Fix : MCQ Media Mismatch fix

--- editor/controllers/mcq-controller.js
+++ editor/controllers/mcq-controller.js
@@ -180,7 +180,7 @@ angular.module('mcqApp', ['org.ekstep.question']).controller('mcqQuestionFormCon
     temp = tempArray.filter(function (element) {
       return element !== undefined;
     });
-    $scope.editMedia = _.isEmpty(temp) ? 0 : _.union($scope.editMedia, temp);
+    $scope.editMedia = _.union($scope.editMedia, temp);
     $scope.mcqFormData.media = _.isEmpty($scope.editMedia[0]) ? temp : $scope.editMedia;
     //check if audio is their then add audio icon in media array
     if ($scope.mcqFormData.media.length > 0) $scope.addDefaultMedia();
